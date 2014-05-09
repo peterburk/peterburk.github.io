746406f0db819c7e6960613563c80a5d4808ba7a
Peter Burkimsher <peterburk@gmail.com>
2014-05-09 19:36:00 +0530

    php redirect

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Redirect browser */
+$url = "http://peterburk.tumblr.com";
+header("Location: $url");
+
+/* Make sure that code below does not get executed when we redirect. */
+exit;
+?>
+ 
+
+
